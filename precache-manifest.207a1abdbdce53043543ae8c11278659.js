0a3e84480e7864803b1b985117dc8ccce0cdcd6e
sumit mishra <sumit.mishra14392@gmail.com>
2020-09-21 02:52:23 +0530

    Updates

---
+++ precache-manifest.207a1abdbdce53043543ae8c11278659.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef9dd48c097f1a6de22f172b262249e",
+    "url": "/Budget-app_reactjs/index.html"
+  },
+  {
+    "revision": "d784ab973a2a144bd24b",
+    "url": "/Budget-app_reactjs/static/css/main.45774326.chunk.css"
+  },
+  {
+    "revision": "30eec6d801e1a3fa9b05",
+    "url": "/Budget-app_reactjs/static/js/2.5e30e283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Budget-app_reactjs/static/js/2.5e30e283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d784ab973a2a144bd24b",
+    "url": "/Budget-app_reactjs/static/js/main.822134db.chunk.js"
+  },
+  {
+    "revision": "0de1f20c4644326605ac",
+    "url": "/Budget-app_reactjs/static/js/runtime-main.42c170d2.js"
+  }
+]);
